cce83c17b6d85513a4bccd8e40cf7929840498fa
KIMPPT <dudghks9303@gmail.com>
2023-05-19 15:38:47 +0900

    05191538

--- firebase/src/database/firebase.js
+++ firebase/src/database/firebase.js
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 //인증을 위한 getAuth 가져오기
-import {getAuth} from 'firebase/auth'
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -21,4 +21,4 @@ const app = initializeApp(firebaseConfig);
 
 //사용하고자 하는 서비스를 들고와서 사용
 //인증서비스에 관한 내용 들고와서 사용
-export const auth=getAuth(app);
+export const auth = getAuth(app);
