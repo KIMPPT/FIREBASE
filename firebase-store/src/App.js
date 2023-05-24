9515f8e44181256424dbeb8e652bc50abdd9545a
KIMPPT <dudghks9303@gmail.com>
2023-05-24 10:16:25 +0900

    05241016

--- firebase-store/src/App.js
+++ firebase-store/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
+import Login from "./page/Login";
 function App() {
-  return <div className="App">
-<Routes>
-  <Route path="/" element={<Home/>}/>
-</Routes>
-  </div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ firebase-store/src/database/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+//인증을 위한 getAuth 가져오기
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: "fir-648aa",
+  storageBucket: "fir-648aa.appspot.com",
+  messagingSenderId: "376439799230",
+  appId: "1:376439799230:web:1afeeed58d867d12948def",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//사용하고자 하는 서비스를 들고와서 사용
+//인증서비스에 관한 내용 들고와서 사용
+export const auth = getAuth(app);
