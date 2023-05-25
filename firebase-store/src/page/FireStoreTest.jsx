361a821b2b458ec40ab924e4538c372c9fa8ff3f
KIMPPT <dudghks9303@gmail.com>
2023-05-25 11:14:32 +0900

    05251114

--- firebase-store/src/page/FireStoreTest.jsx
+++ firebase-store/src/page/FireStoreTest.jsx
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { collection, addDoc, getDocs } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "../database/firebase";
 export default function FireStoreTest() {
   const [users, setUsers] = useState();
+  //가져올 값을 개별 state로 가져오기
+  const [first, setFirst] = useState();
+  const [last, setLast] = useState();
+  const [born, setBorn] = useState();
+  //변경할 값을 state로 가져오기
+  const [updateFirst, setUpdateFirst] = useState();
   //시작하자마자 값 가져오기
   useEffect(() => {
     //비동기함수로 작성하셔 값 가져옴
     getData();
   }, []);
-
+  //getData함수의 이유 : 이벤트를 실행했을 때 실시간으로 추가나 삭제를 보여줌
   async function getData() {
     //getDocs를 통해서 컬렉션 안의 모든 문서를 가져옴
     const querySnapshot = await getDocs(collection(db, "users"));
@@ -19,7 +32,13 @@ export default function FireStoreTest() {
       //doc.id와 doc.data()값을 redux/state에 저장하여
       //웹에서 사용 > forEach의 모든 내용을 배열로 저장
       //배열에 계속해서 추가한다. 안에 set을 넣으면 forEach에 의해 console까지 반복하는데 그러면 앞의 값을 계속해서 덮는다
-      dataArray.push(doc.data()); 
+      //id 값을 함께 넣어주기 위해서 새로운 객체 생성
+      //id는 doc.id, 객체인 doc.data()는 ...(스프레드연산자)를 통해
+      //그 안에 있는 값을 꺼내서 객체로 다시 만들어서 넣음
+      dataArray.push({
+        id: doc.id,
+        ...doc.data(),
+      });
       console.log(`${doc.id} => ${doc.data().admin}`);
     });
     setUsers(dataArray); //이것을 기준으로 화면이 바뀐다
@@ -29,9 +48,9 @@ export default function FireStoreTest() {
     try {
       //서버에 연결해서 사용하는 것은 비동기 함수로 작성(async-await)
       const docRef = await addDoc(collection(db, "users"), {
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815,
+        first,
+        last,
+        born: parseInt(born),
       });
       console.log("Document written with ID: ", docRef.id);
     } catch (e) {
@@ -39,17 +58,45 @@ export default function FireStoreTest() {
     }
     getData();
   };
+  //id값을 가져와서 삭제
+  const deleteData = async (id) => {
+    //doc(db,컬렉션이름,id)로 하나의 문서를 찾을 수 있다
+    await deleteDoc(doc(db, "users", id));
+    getData();
+  };
+  //id값을 가져와서 내용 업데이트
+  const updateData = async (id) => {
+    //수정할 필드의 값을 객체형태로 넣어줌
+    await updateDoc(doc(db, "users", id), {
+      first: updateFirst,
+    });
+    getData();
+  };
   return (
     <div>
       <h3>Firestore의 값을 추가, 가져옴 확인</h3>
       <p>users컬렉션 확인</p>
+      <label htmlFor="">first</label>
+      <input type="text" onChange={(e) => setFirst(e.target.value)} />
+
+      <label htmlFor="">last</label>
+      <input type="text" onChange={(e) => setLast(e.target.value)} />
+
+      <label htmlFor="">born</label>
+      <input type="number" onChange={(e) => setBorn(e.target.value)} />
       <button onClick={addDocData}>버튼을 누르면 firestore에 값 추가</button>
       <br />
       {users &&
         users.map((x) => (
           <div>
             <p>
-              {x.first},{x.born}
+              {x.id},{x.first},{x.born}
+              <button onClick={() => deleteData(x.id)}>X</button>
+              <input
+                type="text"
+                onChange={(e) => setUpdateFirst(e.target.value)}
+              />
+              <button onClick={() => updateData(x.id)}>first 수정</button>
             </p>
           </div>
         ))}
